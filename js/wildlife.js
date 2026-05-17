c2a2b740ce2ed993960ac3280b42f563a9659d71
owenhejna <ohejna@outlook.com>
2026-05-17 17:33:32 -0500

    Added new wildlife sightings

--- js/wildlife.js
+++ js/wildlife.js
@@ -17,7 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
     "Green Bay, WI": [44.5133, -88.0133],
     "Tucson, AZ": [32.2226, -110.9747],
     "Berlin, Germany": [52.5200, 13.4050],
-    "Barcelona, Spain": [41.3851, 2.1734]
+    "Barcelona, Spain": [41.3851, 2.1734],
+    "Guanacaste, Costa Rica": [10.4354, -85.4342],
+    "Great Smoky Mountains, TN": [35.6532, -83.5070],
+    "Indiana Dunes State Park, IN": [41.6531, -87.0631]
   };
 
   // Example data for each sighting
@@ -25,142 +28,191 @@ document.addEventListener("DOMContentLoaded", () => {
     "Ferruginous Pygmy Owl": {
       scientific: "Glaucidium brasilianum",
       description: "Found these diurnal cuties while on a morning walk in Tulum, Mexico. Quite the personality on these two!",
-      img: "../photos/ferriginouspygmyowl.png",
+      img: "../photos/sightings/ferriginouspygmyowl.webp",
       location: "Tulum, Mexico",
       coords: [20.282188221426793, -87.38036160619276]
     },
     "Snowy Owl": {
       scientific: "Bubo scandiacus",
       description: "Caught the famous Montrose snowy owls before work one day! Sorry for the horrendous photo!",
-      img: "../photos/snowyowl.png",
+      img: "../photos/sightings/snowyowl.webp",
       location: "Chicago, IL",
       coords: [41.968818, -87.631523]
     },
     "Greater Roadrunner": {
       scientific: "Geococcyx californianus",
       description: "This guy was running around a canyon in Tuscon, seemingly unbothered by human presence.",
-      img: "../photos/roadrunner.png",
+      img: "../photos/sightings/roadrunner.webp",
       location: "Tuscon, AZ",
       coords: [32.322436761616935, -110.8096851131288]
     },
     "Moose": {
       scientific: "Alces alces",
       description: "Spotted outside a gas station in Jackson, WY. Holy smokes are they huge...",
-      img: "../photos/moose.png",
+      img: "../photos/sightings/moose.webp",
       location: "Jackson, WY",
       coords: [43.585527531177604, -110.82646752477555]
     },
     "Greater White-fronted Goose": {
       scientific: "Anser albifrons",
       description: "I saw this goose in the parking lot on the way into work at the Lincoln Park Zoo. Didn't have to try too hard for this guy!",
-      img: "../photos/greaterwhitefrontedgoose.png",
+      img: "../photos/sightings/greaterwhitefrontedgoose.webp",
       location: "Chicago, IL",
       coords: [41.92361574439163, -87.63247185213538]
     },
     "Common Potoo": {
       scientific: "Nyctibius griseus",
       description: "Saw this guy on the side of the road in rural Costa Rica. Hard to not mistake it for just another fence post.",
-      img: "../photos/potoo.png",
+      img: "../photos/sightings/potoo.webp",
       location: "Costa Rica",
       coords: [9.480850581141752, -84.11876581338923]
     },
     "Lesson's Motmot": {
       scientific: "Momotus lessonii",
       description: "Found this magnificent bird in some jungle near where we were staying in Costa Rica. One of my favorite bird sightings of all time.",
-      img: "../photos/motmot.png",
+      img: "../photos/sightings/motmot.webp",
       location: "Costa Rica",
       coords: [9.422690133743304, -84.16006251304852]
     },
     "American Bison": {
       scientific: "Bison bison",
       description: "Found this massive guy just outside the ranger station in Yellowstone National Park. Seems to be wearing some sort of collar?",
-      img: "../photos/bison.png",
+      img: "../photos/sightings/bison.webp",
       location: "Yellowstone, WY",
       coords: [45.02986083828517, -110.70528993232945]
     },
     "Elk": {
       scientific: "Cervus canadensis",
       description: "Similar to the Bison, this guy was found right outside the visitor's center. Didn't seem to phased by people or cars.",
-      img: "../photos/elk.png",
+      img: "../photos/sightings/elk.webp",
       location: "Yellowstone, WY",
       coords: [44.97467988528676, -110.70002956648177]
     },
     "American Bittern": {
       scientific: "Botaurus lentiginosus",
       description: "Finally got eyes on this secretive marsh bird while leading a volunteer group at Lincoln Park Zoo's Nature Boardwalk. Shout out to the photographer who showed it to us!",
-      img: "../photos/bittern.png",
+      img: "../photos/sightings/bittern.webp",
       location: "Chicago, IL",
       coords: [41.916779059431924, -87.63126429133563]
     },
     "Egyptian Goose": {
       scientific: "Alopochen aegyptiaca",
       description: "Saw these two in Vondelpark while on a European trip with some friends. Also I spotted these guys on my birthday!",
-      img: "../photos/egyptiangeese.png",
+      img: "../photos/sightings/egyptiangeese.webp",
       location: "Amsterdam, Netherlands",
       coords: [52.35763943213383, 4.866430108374714]
     },
     "Long-eared Owl": {
       scientific: "Asio otus",
       description: "Found this owl roosting in some bramble at Montrose. Those ears really do a great job of breaking up the body shape. Sorry for the bad picture!",
-      img: "../photos/longearedowl.png",
+      img: "../photos/sightings/longearedowl.webp",
       location: "Chicago, IL",
       coords: [41.963746902298034, -87.6341576060426]
     },
     "Pileated Woodpecker": {
       scientific: "Dryocopus pileatus",
       description: "My first pileated sighting since I was a young boy. Saw this guy drumming away at an fallen tree. Lots of good bugs for sure!",
-      img: "../photos/pilleated.png",
+      img: "../photos/sightings/pilleated.webp",
       location: "AuSable, MI",
       coords: [44.66833971676804, -84.52465991133202]
     },
     "Mexican Free-tailed Bat": {
       scientific: "Tadarida brasiliensis",
       description: "Found this guy flipped on his back in the sand. Flipped him over and got him to crawl up onto some rock. Hopefully he was able to take back off!",
-      img: "../photos/mexicanfreetailed.png",
+      img: "../photos/sightings/mexicanfreetailed.webp",
       location: "Tulum, Mexico",
       coords: [20.28390320326743, -87.37966048102233]
     },
     "Black-billed Cuckoo": {
       scientific: "Coccyzus erythropthalmus",
       description: "My first Cuckoo sighting! Caught this one at Fonferek's Glen near Green Bay, WI.",
-      img: "../photos/cuckoo.png",
+      img: "../photos/sightings/cuckoo.webp",
       location: "Green Bay, WI",
       coords: [44.42592574755989, -87.93892622280761]
     },
     "Acorn Woodpecker": {
       scientific: "Melanerpes formicivorus",
       description: "Caught one of these amazing woodpeckers on my trip to Arizona. Definitely one of my favorites!",
-      img: "../photos/acornwoodpecker.png",
+      img: "../photos/sightings/acornwoodpecker.webp",
       location: "Tucson, AZ",
       coords: [31.725155932570654, -110.88020645793905]
     },
     "Elegant Trogon": {
       scientific: "Trogon elegans",
-      description: "Saw this Trogan at Sabine Canyon outside of Tuscon. Highly recommended spot for birders if you are in AZ!",
-      img: "../photos/trojon.png",
+      description: "Saw this Trogon at Sabino Canyon outside of Tucson. Highly recommended spot for birders if you are in AZ!",
+      img: "../photos/sightings/trojon.webp",
       location: "Tucson, AZ",
       coords: [31.722365138886246, -110.87997427528668]
     },
     "Cliff Swallow": {
       scientific: "Petrochelidon pyrrhonota",
       description: "Always fun to see these rare visitors building nests over at Lincoln Park Zoo. Check underneath the bridge at Nature Boardwalk during summer to find these guys!",
-      img: "../photos/cliffswallows.png",
+      img: "../photos/sightings/cliffswallows.webp",
       location: "Chicago, IL",
       coords: [41.918194580898586, -87.6329672597927]
     },
     "Mandarin Duck": {
       scientific: "Aix galericulata",
-      description: "Saw this guy floating down a river in Berlin. Spectactular colors, remind me of the Wood Duck in America.",
-      img: "../photos/mandarinduck.png",
+      description: "Saw this guy floating down a river in Berlin. Spectacular colors, remind me of the Wood Duck in America.",
+      img: "../photos/sightings/mandarinduck.webp",
       location: "Berlin, Germany",
       coords: [52.503593487797346, 13.442390567157384]
     },
     "Eurasian Hoopoe": {
       scientific: "Upupa epops",
       description: "Super cool bird with an awesome hairstyle. Found probing the ground for insects near a park in Barcelona.",
-      img: "../photos/hoopoe.png",
+      img: "../photos/sightings/hoopoe.webp",
       location: "Barcelona, Spain",
       coords: [41.42242547375266, 2.2206889287541087]
+    },
+    "Golden Mantled Howler Monkey": {
+      scientific: "Alouatta palliata",
+      description: "Heard these guys long before we saw them — their howls carry for miles through the jungle. Amazing to finally see one up close in Guanacaste!",
+      img: "../photos/sightings/goldenmantledhowlermonkey.webp",
+      location: "Guanacaste, Costa Rica",
+      coords: [10.4354, -85.4342]
+    },
+    "Painted Bunting": {
+      scientific: "Passerina ciris",
+      description: "One of the most spectacular birds I've ever seen. The male's colors look almost too vivid to be real. Spotted in Guanacaste, Costa Rica.",
+      img: "../photos/sightings/paintedbunting.webp",
+      location: "Guanacaste, Costa Rica",
+      coords: [10.4200, -85.4500]
+    },
+    "Gray Sac-winged Bat": {
+      scientific: "Balantiopteryx plicata",
+      description: "Found a small colony roosting on a concrete wall in Guanacaste. Tiny but fascinating little bats!",
+      img: "../photos/sightings/graysacwingedbat.webp",
+      location: "Guanacaste, Costa Rica",
+      coords: [10.4500, -85.4200]
+    },
+    "North American River Otter": {
+      scientific: "Lontra canadensis",
+      description: "Watched this otter fishing in a clear mountain stream in the Smokies. Incredibly graceful in the water!",
+      img: "../photos/sightings/northamericanriverotter.webp",
+      location: "Great Smoky Mountains, TN",
+      coords: [35.6532, -83.5070]
+    },
+    "American Black Bear": {
+      scientific: "Ursus americanus",
+      description: "Came across this bear foraging along the roadside in the Smokies. An iconic sight in this park!",
+      img: "../photos/sightings/americanblackbear.webp",
+      location: "Great Smoky Mountains, TN",
+      coords: [35.6600, -83.4900]
+    },
+    "Spring Salamander": {
+      scientific: "Gyrinophilus porphyriticus",
+      description: "Found this beauty tucked under a rock near a cold mountain stream. The Smokies are one of the salamander capitals of the world!",
+      img: "../photos/sightings/springsalamander.webp",
+      location: "Great Smoky Mountains, TN",
+      coords: [35.6450, -83.5200]
+    },
+    "Prothonotary Warbler": {
+      scientific: "Protonotaria citrea",
+      description: "A stunning golden warbler spotted along the wooded swampy edges of Indiana Dunes State Park during spring migration.",
+      img: "../photos/sightings/prothonotarywarbler.webp",
+      location: "Indiana Dunes State Park, IN",
+      coords: [41.6531, -87.0631]
     }
   };
 
@@ -181,60 +233,47 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // ---- Toggle buttons ----
   gridBtn.addEventListener("click", () => {
-    // Show grid, hide map
     sightingsContainer.className = "grid-view";
-    sightingsContainer.style.display = ""; // Remove inline display style
+    sightingsContainer.style.display = "";
     if (mapContainer) {
       mapContainer.style.display = "none";
     }
-    
     gridBtn.classList.add("active");
     mapBtn.classList.remove("active");
   });
 
   mapBtn.addEventListener("click", () => {
-    // Hide grid, show map
     sightingsContainer.className = "";
     sightingsContainer.style.display = "none";
-    
     if (mapContainer) {
       mapContainer.style.display = "block";
     }
-    
     mapBtn.classList.add("active");
     gridBtn.classList.remove("active");
 
-    // Initialize map only once
     if (!window.myMap && mapContainer) {
       window.myMap = L.map('map').setView([41.8781, -87.6298], 3);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
       }).addTo(window.myMap);
+
       const greenPin = new L.Icon({
-  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
-  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
+        iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
 
-
-// Add markers for all sightings
-Object.entries(sightingData).forEach(([species, data]) => {
-  if (data.coords) {
-const marker = L.marker(data.coords, {
-  icon: greenPin
-}).addTo(window.myMap);
-
-
-marker.bindPopup(`<b>${species}</b><br>${data.location}`);
-
-  }
-});
+      Object.entries(sightingData).forEach(([species, data]) => {
+        if (data.coords) {
+          const marker = L.marker(data.coords, { icon: greenPin }).addTo(window.myMap);
+          marker.bindPopup(`<b>${species}</b><br>${data.location}`);
+        }
+      });
     } else if (window.myMap) {
-      // If map already exists, invalidate size to fix display issues
       setTimeout(() => {
         window.myMap.invalidateSize();
       }, 100);
